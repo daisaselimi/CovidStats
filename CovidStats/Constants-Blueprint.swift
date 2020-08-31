e20f8cf510eb6d3a6a558335e25195c549382af3
daisaselimi <da.isaselimi@gmail.com>
2020-08-31 18:24:43 +0200

    Add Constants file blueprint and remove the main one

---
+++ CovidStats/Constants-Blueprint.swift
@@ -0,0 +1,21 @@
+//
+//  Constants-Blueprint.swift
+//  CovidStats
+//
+//  Created by Isa  Selimi on 31.8.20.
+//  Copyright © 2020 com.isaselimi. All rights reserved.
+//
+
+import Foundation
+//
+//struct RapidApiContstants {
+//    
+//    static let host = "x-rapidapi-host"
+//    static let key = "x-rapidapi-key"
+//    static let hostValue = "covid-19-data.p.rapidapi.com"
+//    static let keyValue =  ""
+//    static let personalKeyValue = ""
+//    
+//}
+//
+//let lastFetchedFromWeb = "lastFetchedFromWeb"
